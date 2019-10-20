7809b8dae08b1a4afbec0d565f3ef95f2950261d
eraute200 <emily_rautenberg@comcast.com>
2019-10-20 18:58:44 -0400

    Step 6: Character Sets and Word Boundaries

---
+++ src/6_characters.js
@@ -0,0 +1,81 @@
+/* Character Groups */
+
+export function characters() {
+  // There are many different character codes that can be used as shorthands for groupings of characters
+  //   \d - any digit character (0-9)
+  //   \w - any alphanumeric character (a-z, A-Z, 0-9, and some symbols)
+  //   \s - any whitespace character (space, tab, etc.)
+  //   \D - any NON-digit character
+  //   \W - any NON-alphanumeric character
+  //   \S - any NON-whitespace character
+  //   . - any character that isn't a newline (\n)
+
+  var str = "This contains all 6 types of characters.";
+
+  char_nums(str);
+  char_words(str);
+  char_space(str);
+  char_noNums(str);
+  char_noWords(str);
+  char_noSpace(str);
+  char_all(str);
+  char_boundary(str);
+}
+
+function char_nums(str) {
+  var nums = /\d/g; // search for any digits
+  var numsMatch = str.match(nums);
+  console.log(`${nums}:`, numsMatch);
+}
+
+function char_words(str) {
+  var words = /\w/g;  // search for any word characters
+  var wordsMatch = str.match(words);
+  console.log(`${words}:`, wordsMatch);
+}
+
+function char_space(str) {
+  var space = /\s/g; // search for whitespaces
+  var spaceMatch = str.match(space);
+  console.log(`${space}:`, spaceMatch);
+}
+
+function char_noNums(str) {
+  var nonNums = /\D/g; // search for non digits
+  var nonNumsMatch = str.match(nonNums);
+  console.log(`${nonNums}:`, nonNumsMatch);
+}
+
+function char_noWords(str) {
+  var nonWords = /\W/g; // search for non word characters
+  var nonWordsMatch = str.match(nonWords);
+  console.log(`${nonWords}:`, nonWordsMatch);
+}
+
+function char_noSpace(str) {
+  var nonSpace = /\S/g; // search for non white spaces
+  var nonSpaceMatch = str.match(nonSpace);
+  console.log(`${nonSpace}:`, nonSpaceMatch);
+}
+
+function char_all(str) {
+  var all = /./g; // search for any non newline character
+  var allMatch = str.match(all);
+  console.log(`${all}:`, allMatch);
+}
+
+function char_boundary() {
+  // There is one more character class called a "word boundary"
+
+  var boundaryStr = "Java has a boundary, but JavaScript does not";
+  console.log(boundaryStr);
+
+  var boundary = /\bJava\b/g;
+  var boundaryMatch = boundaryStr.search(boundary);
+  console.log(`${boundary}:`, boundaryMatch);
+
+  // It also has a reverse
+  var nonBoundary = /Java\B/g;
+  var nonBoundaryMatch = boundaryStr.search(nonBoundary);
+  console.log(`${nonBoundary}:`, nonBoundaryMatch);
+}
--- src/index.js
+++ src/index.js
@@ -3,31 +3,32 @@ import * as steptwo   from './2_methods';
 import * as stepthree from './3_flags';
 import * as stepfour  from './4_anchors';
 import * as stepfive  from './5_setsranges';
+import * as stepsix  from './6_characters';
 
 function showStep(step) {
   switch (step) {
     case 1:
     default:
       // Step One: Creating a new RegEx
-      console.log('\nCREATING A NEW REGEX');
+      console.log('\nCREATING A NEW REGEX\n\n');
       stepone.createRegEx();
       break;
 
     case 2:
       // Step Two: Methods of RegEx and Strings
-      console.log('\nMETHODS OF REGEX AND STRINGS');
+      console.log('\nMETHODS OF REGEX AND STRINGS\n\n');
       steptwo.methods();
       break;
 
     case 3:
       // Step Three: Flags
-      console.log('\nFLAGS');
+      console.log('\nFLAGS\n\n');
       stepthree.flags();
       break;
 
     case 4:
       // Step Four: Flags
-      console.log('\nANCHORS');
+      console.log('\nANCHORS\n\n');
       stepfour.anchors();
       break;
 
@@ -36,7 +37,12 @@ function showStep(step) {
       console.log('\nSETS AND RANGES\n\n');
       stepfive.setsranges();
       break;
+
+    case 6:
+      // Step Six: Character Sets
+      console.log('\nCHARACTER SETS\n\n');
+      stepsix.characters();
   }
 }
 
-showStep(5);
+showStep(6);
