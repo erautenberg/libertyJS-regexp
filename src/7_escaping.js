7df74518998a19c0b6a62f8434e9cba4184216c8
eraute200 <emily_rautenberg@comcast.com>
2019-10-20 19:05:15 -0400

    Step 8: Escaping Special Characters

---
+++ src/7_escaping.js
@@ -0,0 +1,14 @@
+/* Escaping Special Characters */
+
+export function special() {
+  // As you probably noticed by now, RegEx uses special characters to declare specific behavior
+  // If the pattern you are looking for actually contains a special character,
+  // similarly to HTML, Javascript, etc., you will have to escape it using the backslash, '\'
+  var reg = /[\.\?\!\(\)]/g;
+
+  var str = "How many special characters ('.', '?', '!', '(', ')') are in this sentence?";
+  var match = str.match(reg);
+
+  console.log(str);
+  console.log(`${reg}:`, match);
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import * as stepone   from './1_create';
-import * as steptwo   from './2_methods';
-import * as stepthree from './3_flags';
-import * as stepfour  from './4_anchors';
-import * as stepfive  from './5_setsranges';
-import * as stepsix  from './6_characters';
+import * as step1 from './1_create';
+import * as step2 from './2_methods';
+import * as step3 from './3_flags';
+import * as step4 from './4_anchors';
+import * as step5 from './5_setsranges';
+import * as step6 from './6_characters';
+import * as step7 from './7_escaping';
 
 function showStep(step) {
   switch (step) {
@@ -11,38 +12,44 @@ function showStep(step) {
     default:
       // Step One: Creating a new RegEx
       console.log('\nCREATING A NEW REGEX\n\n');
-      stepone.createRegEx();
+      step1.createRegEx();
       break;
 
     case 2:
       // Step Two: Methods of RegEx and Strings
       console.log('\nMETHODS OF REGEX AND STRINGS\n\n');
-      steptwo.methods();
+      step2.methods();
       break;
 
     case 3:
       // Step Three: Flags
       console.log('\nFLAGS\n\n');
-      stepthree.flags();
+      step3.flags();
       break;
 
     case 4:
       // Step Four: Flags
       console.log('\nANCHORS\n\n');
-      stepfour.anchors();
+      step4.anchors();
       break;
 
     case 5:
       // Step Five: Sets and Ranges
       console.log('\nSETS AND RANGES\n\n');
-      stepfive.setsranges();
+      step5.setsranges();
       break;
 
     case 6:
       // Step Six: Character Sets
       console.log('\nCHARACTER SETS\n\n');
-      stepsix.characters();
+      step6.characters();
+
+    case 7:
+      // Step Seven: Escaping Special Characters
+      console.log('\nESCAPING SPECIAL CHARACTERS\n\n');
+      step7.special();
+
   }
 }
 
-showStep(6);
+showStep(7);
